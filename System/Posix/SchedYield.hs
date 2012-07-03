d7a4dc5fbd5aef02b55a964c5fb168b29419fff3
Michal J. Gajda <mjgajda@mail.google.com>
2012-07-03 07:55:11 +0200

    Windows compatibility.

--- System/Posix/SchedYield.hs
+++ System/Posix/SchedYield.hs
@@ -6,13 +6,13 @@ where
 import Foreign.C.Types(CInt(..))
 import Foreign.C.Error(throwErrnoIf_)
 
-#ifndef __WINDOWS__
 -- | c_sched_yield is internal FFI entry, without any wrapping of errno value as exception.
-foreign import ccall "sched.h sched_yield"
+#ifdef mingw32_HOST_OS
+foreign import ccall "Windows.h SwitchToThread"
     c_sched_yield :: IO CInt
 #else
-foreign import ccall "Windows.h SwitchToThread"
-    c_sched_yield :: IO CBool "Windows.h"
+foreign import ccall "sched.h sched_yield"
+    c_sched_yield :: IO CInt
 #endif
 
 -- | sched_yield yields processor and waits until all other processes, with a comparable or better nice value, could execute at least one time slice.
